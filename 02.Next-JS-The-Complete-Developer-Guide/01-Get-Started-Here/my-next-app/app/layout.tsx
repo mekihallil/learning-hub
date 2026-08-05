1d741b7f6594d4a49fa21cf68d67634a4b8e04a4
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-05 14:55:06 +0300

    feat: link all dashboard pages in root layout

--- 02.Next-JS-The-Complete-Developer-Guide/01-Get-Started-Here/my-next-app/app/layout.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/01-Get-Started-Here/my-next-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col" cz-shortcut-listen="true" >{children}</body>
+      <body className="min-h-full flex flex-col" cz-shortcut-listen="true" >
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
+        {children}</body>
     </html>
   );
 }
