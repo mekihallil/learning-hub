2f9e973356c75035a7631589ed7e9f0bab8ac9b6
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-18 16:11:26 +0300

    chore: cleanup boilerplate files and styles

--- 02.Next-JS-The-Complete-Developer-Guide/02-Changing-Data-with-Mutations/snippets/app/layout.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/02-Changing-Data-with-Mutations/snippets/app/layout.tsx
@@ -23,10 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <h1>Next js</h1>
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
