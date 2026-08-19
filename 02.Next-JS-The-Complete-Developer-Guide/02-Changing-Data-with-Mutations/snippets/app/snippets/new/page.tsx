8c99293afb09c72dfe73a5d20554d804b51994b2
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-19 21:04:13 +0300

    feat: implement create snippet server action with validation and redirect

--- 02.Next-JS-The-Complete-Developer-Guide/02-Changing-Data-with-Mutations/snippets/app/snippets/new/page.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/02-Changing-Data-with-Mutations/snippets/app/snippets/new/page.tsx
@@ -1,6 +1,27 @@
+import { redirect } from "next/navigation";
+import { db } from "../../lib/db";
+
 const SnippetCreatePage = () => {
+  async function createSnippet(formDate: FormData) {
+    // This needs tobe a server action!
+    "use server";
+    // Check the user's inputes and make sure they're valid
+    const title = formDate.get("title") as string;
+    const code = formDate.get("code") as string;
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+    // Redirect the user back to the root router
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
