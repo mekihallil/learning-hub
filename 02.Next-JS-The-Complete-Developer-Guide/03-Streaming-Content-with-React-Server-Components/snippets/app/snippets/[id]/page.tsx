8a8ad04720194f735d8adea5cdd11d173dd3ac53
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-21 16:20:32 +0300

    feat: use dynamic route params to fetch record-specific data from database

---
+++ 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/snippets/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import { db } from "../../lib/db";
+
+interface snippetShowPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+export default async function snippetShowPage({
+  params,
+}: snippetShowPageProps) {
+  // Get the ID from params
+  const { id } = await params;
+
+  //   Fetch the PARTICULAR record from database
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  //   If no snippet  is found, show 404 page
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>{snippet.title}</div>;
+}
