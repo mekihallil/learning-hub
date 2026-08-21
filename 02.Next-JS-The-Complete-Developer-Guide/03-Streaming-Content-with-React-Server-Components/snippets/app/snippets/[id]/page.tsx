841d7f7686ec5d5d24c27c56bc95eadd6a43e80c
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-21 23:00:06 +0300

    style: add layout and button styling to snippet show page

--- 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/snippets/[id]/page.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/snippets/[id]/page.tsx
@@ -11,15 +11,30 @@ export default async function snippetShowPage({
 }: snippetShowPageProps) {
   // Get the ID from params
   const { id } = await params;
-  await new Promise((r) => setTimeout(r, 2000));
-  //   Fetch the PARTICULAR record from database
+
+  // Fetch the PARTICULAR record from database
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(id) },
   });
 
-  //   If no snippet  is found, show 404 page
+  // If no snippet  is found, show 404 page
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet.title}</div>;
+
+  const { title, code } = snippet;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{code}</code>
+      </pre>
+    </div>
+  );
 }
