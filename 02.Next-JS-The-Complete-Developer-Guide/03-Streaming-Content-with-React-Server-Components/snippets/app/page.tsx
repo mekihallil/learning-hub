441295dcc756ea022ed3b05bd54a90a3852b5a35
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-21 14:00:51 +0300

    feat: implement server component data fetching using prisma findMany

--- 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/page.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from "./lib/db";
+
+export default async function Home() {
+  // Fetching data from databse
+  const snippets = await db.snippet.findMany();
+
+  // map over the data and display it
+  const renderedSnippets = snippets.map(({ id, title }) => {
+    return <div key={id}>{title}</div>;
+  });
+  return <div>{renderedSnippets}</div>;
 }
