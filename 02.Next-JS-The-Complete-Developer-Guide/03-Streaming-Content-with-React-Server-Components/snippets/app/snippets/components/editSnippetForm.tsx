6feaff72a978b1844603f94a5e937a665a47c3df
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-23 15:02:09 +0300

    feat: implement code editing using @monaco-editor/react

--- 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/snippets/components/editSnippetForm.tsx
+++ 02.Next-JS-The-Complete-Developer-Guide/03-Streaming-Content-with-React-Server-Components/snippets/app/snippets/components/editSnippetForm.tsx
@@ -1,12 +1,23 @@
-"user client";
+"use client";
 
 import { Snippet } from "@/generated/prisma/client";
+import { Editor } from "@monaco-editor/react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function snippetEditForm({ snippet }: SnippetEditFormProps) {
-  const { title } = snippet;
-  return <div>Client component has snippet with title {title}</div>;
+  const { code } = snippet;
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={code}
+        options={{ minimap: { enabled: false } }}
+      />
+    </div>
+  );
 }
