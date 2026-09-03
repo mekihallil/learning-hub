fa2ce382bf3db8dbc2c00683403b368f735fac3c
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-09-03 11:20:01 +0300

    chore: set up practice project for useFormState hooks

---
+++ 02.Next-JS-The-Complete-Developer-Guide/05-Server-Forms-with-the-UseFormState-Hook/snippets/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import SnippetEditForm from "@/snippets/components/editSnippetForm";
+import { db } from "@/lib/db";
+
+interface snippetPropsId {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function snippetEditPage({ params }: snippetPropsId) {
+  const snippetId = await params;
+  const id = parseInt(snippetId.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+  if (!snippet) {
+    return notFound;
+  }
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+}
---
+++ 02.Next-JS-The-Complete-Developer-Guide/05-Server-Forms-with-the-UseFormState-Hook/snippets/app/snippets/components/editSnippetForm.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Snippet } from "@/generated/prisma/client";
+import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
+import { editSnippet } from "../action/editSnippet";
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const { id, code } = snippet;
+  const [updatedCode, setCode] = useState(code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  const handleClick = async () => {
+    await editSnippet(id, updatedCode);
+  };
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+      <div>
+        <button
+          onClick={handleClick}
+          className="border rounded p-2 mt-2 cursor-pointer"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
